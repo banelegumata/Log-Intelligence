581f28bb5b7d9abc5d46f7f64badabb151134f53
banelegumata <b.sgumata@gmail.com>
2026-08-28 17:45:05 +0200

    First Initial commit for Log Intelligence Tool. configured H2 in memory db, application properties file, and added dependencies

---
+++ src/main/java/za/co/banelegumata/logintelligence/log_intelligence_tool/LogIntelligenceToolApplication.java
@@ -0,0 +1,13 @@
+package za.co.banelegumata.logintelligence.log_intelligence_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogIntelligenceToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogIntelligenceToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/banelegumata/logintelligence/log_intelligence_tool/LogIntelligenceToolApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.banelegumata.logintelligence.log_intelligence_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogIntelligenceToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
